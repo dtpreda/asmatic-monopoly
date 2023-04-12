6b6d7e1640575498dc4a6ea5811a93ed349ebb31
David Preda <dtpreda30@gmail.com>
2023-04-13 00:39:46 +0100

    Add Landlord Strategy

--- src/main/java/monopoly/agents/tradeStrategy/LandlordTradeStrategy.java
+++ src/main/java/monopoly/agents/tradeStrategy/LandlordTradeStrategy.java
@@ -1,12 +1,37 @@
 package monopoly.agents.tradeStrategy;
 
+import jade.content.ContentElement;
 import jade.content.ContentManager;
+import jade.content.lang.Codec;
+import jade.content.onto.OntologyException;
 import jade.lang.acl.ACLMessage;
+import monopoly.actions.ProposeTrade;
 
 public class LandlordTradeStrategy implements TradeStrategy {
     @Override
     public ACLMessage processTrade(ContentManager contentManager, ACLMessage message) {
+        ContentElement content = null;
+        try {
+            content = contentManager.extractContent(message);
+        } catch (Codec.CodecException | OntologyException e) {
+            throw new RuntimeException(e);
+        }
 
-        return null;
+        ProposeTrade trade = (ProposeTrade) content;
+
+        ACLMessage reply = message.createReply();
+
+        if (trade.getTrade().getProperty().getBuilding() > 0) {
+            reply.setPerformative(ACLMessage.REFUSE);
+        } else {
+            reply.setPerformative(ACLMessage.PROPOSE);
+            try {
+                contentManager.fillContent(reply, content);
+            } catch (Codec.CodecException | OntologyException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return reply;
     }
 }
